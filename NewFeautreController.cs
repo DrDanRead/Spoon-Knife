884337f8ce73bd427249a80211a3a6c02aa69768
Daniel Read <daniel.read@schoolcomms.co.uk>
2018-11-28 14:02:06 +0000

    added newfeature class and added/removed from index.html

---
+++ NewFeautreController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class NewFeautreController
+{
+	public NewFeautreController()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
